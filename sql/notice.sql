5d391b7ab09c92f4b03a5cbd4820cb6efaa1b727
Rogits <rawgy2@gmail.com>
2014-07-24 18:09:57 -0700

    initial

---
+++ sql/notice.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE noticesystem;
+USE noticesystem;
+CREATE TABLE notice (A
+  id int(11) NOT NULL auto_increment,
+  title varchar(100) NOT NULL,
+  description text NOT NULL,
+  PRIMARY KEY (id)
+);
+INSERT INTO notice (title, description)
+    VALUES  ('The  Military  Wives',  'In  My  Dreams');
+INSERT INTO  notice (title, description)
+    VALUES  ('Adele',  '21');
+INSERT INTO notice (title, description)
+    VALUES  ('Bruce  Springsteen',  'Wrecking Ball (Deluxe)');
+INSERT INTO notice (title, description)
+    VALUES  ('Lana  Del  Rey',  'Born  To  Die');
+INSERT INTO notice (title, description)
+    VALUES  ('Gotye',  'Making  Mirrors');
